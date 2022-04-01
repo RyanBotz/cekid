b7623d877601799ebdf1840eebbb0f2ff2f3e3c9
RyanBotz <98251598+RyanBotz@users.noreply.github.com>
2022-04-01 01:22:10 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chek id</title>
+</head>
+<body>
+    <a href="ff.php">Freefire</a>
+    <a href="ml.php">Mobile Legend</a>
+</body>
+</html>
